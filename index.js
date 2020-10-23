fe7540f3dd8be14dbf07dbc50cd0baed1b9acf44
nicoContigliani <nico.contigliani@gmail.com>
2020-10-23 20:07:18 -0300

    bloqea

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const url = "https://pokeapi.co/api/v2/pokemon/";
 const idPokemons = [];
 const capturados = [];
 
+
+
+
 var btnAceptar = document.getElementById("listar");
 var disableButton = function () { this.disabled = true; };
 btnAceptar.addEventListener('click', disableButton, false);
@@ -19,7 +22,7 @@ const listaHabilidades = "";
 
 
 (() => {
-   
+
     // while (capturados.length < 7) {
     //     console.log(text)
 
@@ -64,7 +67,7 @@ const listaHabilidades = "";
                 resultado.innerHTML += ` <li>${poke.nombre}- id: ${poke.id}</li>`
             }
 
-     
+
 
             if (resultado.innerHTML === '') {
                 resultado.innerHTML += `<span> Pokemon no encontrado. Desea buscar en la red???</span> `
@@ -93,16 +96,16 @@ const listaHabilidades = "";
 
 const buscarRed = async (texto) => {
     alert(texto)
-    const response = await axios.get(url+texto);
+    const response = await axios.get(url + texto);
     console.log(response.data.forms[0].name);
-    
+
     resultado.innerHTML = `${response.data.forms[0].name}  Desdea agregarlo a las opciones ?         
     <button type="button" class="btn btn-outline-primary" onclick="agregar(${texto});">Agregar</button>
 <button type="button" class="btn btn-outline-secondary">Cancelar</button>  `;
 
 }
 
-const agregar = async (texto)=>{
+const agregar = async (texto) => {
     alert(texto);
     const response = await axios.get(`${url}${texto}`);
     console.log(response.data.forms[0].name + response.data.id);
@@ -110,22 +113,23 @@ const agregar = async (texto)=>{
     const nombre = response.data.forms[0].name;
     const id = response.data.id;
 
-    console.log({id: `${id}`, nombre: `${nombre}` })
+    console.log({ id: `${id}`, nombre: `${nombre}` })
     n = idPokemons.push({ id: `${id}`, nombre: `${nombre}` });
-    mostrar(id,nombre);
+    mostrar(id, nombre);
 }
-const mostrar = async(id,nombre)=>{
+const mostrar = async (id, nombre) => {
     idPokemons.forEach(element => {
- if (id == element.id && nombre ==element.nombre){
-     alert("esta insertado");
-  //   console.log(`id :${element.id}, nombre: ${element.nombre}`)  ;
-     resultado.innerHTML = `Esta insertado  id :${element.id}, nombre: ${element.nombre}   <button type="button" class="btn btn-outline-primary" >Capturar</button>  `
+        if (id == element.id && nombre == element.nombre) {
+            alert("esta insertado");
+            //   console.log(`id :${element.id}, nombre: ${element.nombre}`)  ;
+            resultado.innerHTML = `Esta insertado  id :${element.id}, nombre: ${element.nombre}   <button type="button" class="btn btn-outline-primary" >Capturar</button>  `
 
-     }else{
-        // alert(`${id} y ${element.id} ---   ${nombre} y ${element.nombre}     `);       
-     }
- }
-)};
+        } else {
+            // alert(`${id} y ${element.id} ---   ${nombre} y ${element.nombre}     `);       
+        }
+    }
+    )
+};
 
 //console.log(idPokemons)
 const getPokemons = async () => {
@@ -137,7 +141,7 @@ const getPokemons = async () => {
 
 
         n = idPokemons.push({ id: `${id}`, nombre: `${response.data.forms[0].name}` });
-        console.log({id: `${id}`, nombre: `${response.data.forms[0].name}` })
+        console.log({ id: `${id}`, nombre: `${response.data.forms[0].name}` })
         pAbilities(response.data, id);
 
     }
@@ -145,7 +149,7 @@ const getPokemons = async () => {
 
 
 
-const pAbilities = (data, id) => {
+const pAbilities = async (data, id) => {
     //   data.abilities.forEach(ability=> console.log((id,ability.ability.name)) )
     let name = document.createElement("h2");
     let experiencia = document.createElement("li");
@@ -199,29 +203,53 @@ const pAbilities = (data, id) => {
     var att = document.createAttribute("class");
     var attr = document.createAttribute("onclick");
     var identidad = document.createAttribute("id");
-    // var desabilitar = document.createAttribute("disable")
+    var desabilitar = document.createAttribute("disabled")
+    var nameatt = document.createAttribute("name");
 
 
-    attr.value = "capturar(value)";
+
+
+    att.value = `btn btn-outline-secondary `;
+    boton.setAttributeNode(att);
+
+    attr.value = `capturar(value);  ((att) => {
+        var att = document.createAttribute("class");
+
+        alert("pasa")
+       att.value =  this.disabled = true ;
+    })()`;
+    // boton.setAttributeNode(attrB);
+
     boton.setAttributeNode(attr);
 
-    att.value = "btn btn-outline-secondary";
-    boton.setAttributeNode(att);
+    const change = (att) => {
+        alert("pasa")
+        //att.value = "btn btn-outline-secondary disabled";
+    }
+
 
     identidad.value = `id`;
     boton.setAttributeNode(identidad)
 
-    // desabilitar.value="false"
-    // boton.setAttributeNode(desabilitar)
 
-    identidad.innerHTML=`id`;
-    identidad.value=`${id}`
+
+    nameatt.innerHTML = "name";
+    nameatt.value = "capturar"
+    boton.setAttributeNode(nameatt)
+
+
+    identidad.innerHTML = `id`;
+    identidad.value = `${id}`
 
     boton.innerHTML = "capturar";
     boton.value = `${id}`
 
-    
+
+
     document.body.appendChild(boton);
+
+
+
 
     // document.getElementById("demo").appendChild(boton);
 
@@ -234,31 +262,53 @@ const tm = async () => {
 
 }
 
-const capturar =async(value) =>{
-
-
-    
- await alert(value)
- const response = await axios.get(`${url}${value}`);
- const name=response.data.forms[0].name;
- const experience = response.data.base_experience;
-
- response.data.types.forEach(types =>insertTypess.push(`${types.type.name}`))
-const type  = insertTypess.join(",")
-console.log(type)
-
-response.data.abilities.forEach(ability => insertAbilitiess.push`${ability.ability.name}`)
-const abilities  = insertAbilitiess.join("");
-console.log(abilities)
+const capturar = async (value) => {
 
 
 
+    await alert(value)
+    const response = await axios.get(`${url}${value}`);
+    const name = response.data.forms[0].name;
+    const experience = response.data.base_experience;
 
- console.log(`${name} /// ${experience} /// ${type} ///${abilities}`)
- capturados.push(value);
+    response.data.types.forEach(types => insertTypess.push(`${types.type.name}`))
+    const type = insertTypess.join(",")
+    console.log(type)
 
-// capturados.forEach(element=>{
-//     console.log(element)
-// })
+    response.data.abilities.forEach(ability => insertAbilitiess.push`${ability.ability.name}`)
+    const abilities = insertAbilitiess.join("");
+    console.log(abilities)
 
+    /// aca se pincha la base ..... 
+    console.log(`${name} /// ${experience} /// ${type} ///${abilities}`)
+    capturados.push(value);
+
+    //aca voy a bloquear botones 
+    bloquear(value)
+
+    // capturados.forEach(element=>{
+    //     console.log(element)
+    // })
+
+}
+
+
+
+((value) => {
+
+
+
+
+})();
+const bloquear = (value) => {
+
+    alert("ehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+
+
+
+
+
+
+    // desabilitar.value="disabled"
+    // boton.setAttributeNode(desabilitar)
 }
